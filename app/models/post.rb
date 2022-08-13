e1365b2264e0508fa5369914d7cc765d10c92d5a
MaganaAsiati <assia.magana@gmail.com>
2022-08-13 16:06:44 +0200

    add validation for Title to not be blank

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
+  # validations
+
+  
   def recent_comments
     comments.limit(5).order(created_at: :desc)
   end
 
-  after_save :updates_posts_counter
-
-  private
-
-  def updates_posts_counter
-    posts.increment!(:Posts_Counter)
-  end
-end
+  
