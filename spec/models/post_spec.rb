e8014612d8607b0d736f80107c7763de69c62091
MaganaAsiati <assia.magana@gmail.com>
2022-08-13 22:13:06 +0200

    add test for methode recent_post and update data

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,33 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject { Post.create(author_id: 1, Title: 'Hello', Text: 'This my first post') }
-  before { subject.save }
-
-  it 'title should be not blank' do
-    subject.Title = nil
-    expect(subject).to_not be_valid
+  before :each do
+    @author = User.create(Name: 'Tom', Photo:'https://unsplash.com/photos/F_-0BxGuVvo',
+                          Bio: 'Teacher from Mexico.', Posts_Counter: 0 )
   end
 
-  it 'title should be not blank' do
-    subject.Title = 't' * 251
-    expect(subject).to_not be_valid
-  end
-  it 'CommentsCounter must be an integer ' do
-    subject.Comments_Counter = 'text'
-    expect(subject).to_not be_valid
-  end
-  it 'CommentsCounter must be greater than or equal to zero ' do
-    subject.Comments_Counter = -1
-    expect(subject).to_not be_valid
+    it 'is valid with valid attributes' do
+      expect(Post.create(author:@author, Title: 'Hello', Text: 'This is my first post', Comments_Counter: 0, Likes_Counter: 0)).to be_valid
+    end
+  
+ 
+
+  it 'is not valid with title empty' do
+    expect(Post.create(author:@author, Title: '', Text: 'This is my first post', Comments_Counter: 0, Likes_Counter: 0)).to_not be_valid
   end
 
-  it 'LikesCounter must be an integer greater than or equal to zero. ' do
-    subject.Likes_Counter = 'word'
-    expect(subject).to_not be_valid
+  it 'is not valid with title exceeding 250 characters ' do
+    Title = 'r' * 251
+    expect(Post.create(author:@author, Title:, Text: 'This is my first post', Comments_Counter: 0, Likes_Counter: 0)).to_not be_valid
   end
-  it 'LikesCounter must be an integer greater than or equal to zero. ' do
-    subject.Likes_Counter = -1
-    expect(subject).to_not be_valid
+
+  it 'should update post counter' do
+    Post.create(author:@author, Title: 'Hello', Text: 'This is my first post', Comments_Counter: 0, Likes_Counter: 0)
+    expect(@author.Posts_Counter).to eq(1)
+  end
+
+  it 'should return five (5) most recent comments' do
+    post = Post.create(author:@author, Title: 'Hello', Text: 'This is my first post', Comments_Counter: 6, Likes_Counter: 0)
+    Comment.create(post:, author: @author, text: 'Hi Tom!')
+    Comment.create(post:, author: @author, text: 'Hi Tom!')
+    Comment.create(post:, author: @author, text: 'Hi Tom!')
+    Comment.create(post:, author: @author, text: 'Hi Tom!')
+    Comment.create(post:, author: @author, text: 'Hi Tom!')
+    Comment.create(post:, author: @author, text: 'Hi Tom!')
+
+    expect(post.recent_comments.count).to eq(5)
   end
 end
