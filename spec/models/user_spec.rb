c490aef587bc5a174998cbc77f3f71c10a3ac0b4
MaganaAsiati <assia.magana@gmail.com>
2022-08-13 22:12:22 +0200

    add test for methode recent_comment and update data

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.create(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Mexico.') }
-  before { subject.save }
+  it 'is valid with valid attributes ' do
+    expect(User.create(Name: 'Tom',   Photo: 'https://unsplash.com/Photos/F_-0BxGuVvo',
+                       Bio: 'Teacher from Mexico.', Posts_Counter: 0)).to be_valid
+  end
 
-  it 'Name should not be blank' do
-    subject.Name = nil
-    expect(subject).to_not be_valid
+  it 'is not valid with Name empty ' do
+    expect(User.create(Name: '', Photo: 'https://unsplash.com/Photos/F_-0BxGuVvo',
+                       Bio: 'Teacher from Mexico.')).to_not be_valid
   end
-  it 'PostsCounter must be an integer ' do
-    subject.Posts_Counter = -1
-    expect(subject).to_not be_valid
+
+  it 'is not valid with Posts_Counter nil' do
+    expect(User.create(Name: 'Tom', Photo: 'https://unsplash.com/Photos/F_-0BxGuVvo',
+                       Bio: 'Teacher from Mexico.', Posts_Counter: nil)).to_not be_valid
   end
-  it 'PostsCounter must be greater than or equal to zero. ' do
-    subject.Posts_Counter = 'word'
-    expect(subject).to_not be_valid
+
+  it 'is not valid with Posts_Counter of type string' do
+    expect(User.create(Name: 'Tom', Photo: 'https://unsplash.com/Photos/F_-0BxGuVvo',
+                       Bio: 'Teacher from Mexico.', Posts_Counter: 'nil')).to_not be_valid
+  end
+
+  it 'should return 3 posts' do
+    author = User.create(Name: 'Tom', Photo: 'https://unsplash.com/Photos/F_-0BxGuVvo',
+                         Bio: 'Teacher from Mexico.', Posts_Counter: 0 )
+    Post.create(author:, Title: 'Hello', Text: 'This is my first post', Comments_Counter: 0, Likes_Counter: 0)
+    Post.create(author:, Title: 'Hello', Text: 'This is my first post', Comments_Counter: 0, Likes_Counter: 0)
+    Post.create(author:, Title: 'Hello', Text: 'This is my first post', Comments_Counter: 0, Likes_Counter: 0)
+    Post.create(author:, Title: 'Hello', Text: 'This is my first post', Comments_Counter: 0, Likes_Counter: 0)
+
+    expect(author.recent_posts.count).to eq(3)
   end
 end
