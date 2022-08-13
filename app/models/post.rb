51010540cc6a1669defe716b2682d2356206ff5a
MaganaAsiati <assia.magana@gmail.com>
2022-08-13 16:07:14 +0200

    add validation for Title to not be blank

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,10 @@ class Post < ApplicationRecord
 
   # validations
 
-  
+  validates :Title, presence: true, length: { maximum: 250 }
+  validates :Comments_Counter, :Likes_Counter, numericality: { only_integer: true },
+                                               comparison: { greater_than_or_equal_to: 0 }
+
   def recent_comments
     comments.limit(5).order(created_at: :desc)
   end
