dc9b27a0ebdfb6690aa8d787749fd5b733a4b70a
MaganaAsiati <assia.magana@gmail.com>
2022-08-13 16:03:53 +0200

    add validation for PostsCounter to be an integer greater than or equal to zero

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   has_many :comments, class_name: 'Comment'
   has_many :likes, class_name: 'Like'
   # validations
-    def recent_posts
+  validates :Name, presence: true
+  validates :Posts_Counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  def recent_posts
     posts.limit(3).order(created_at: :desc)
   end
 end
