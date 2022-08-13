a3cd8e1e59c429b8d90da84d9542edc824d290ba
MaganaAsiati <assia.magana@gmail.com>
2022-08-13 12:39:14 +0200

    make methode for update private and call them back

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ApplicationRecord
 
   after_save :update_comment_counter
 
+  private
+
   def update_comment_counter
     post.increment!(:Comments_Counter)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
-  def updates_posts_counter
-    posts.increment!(:Posts_Counter)
-  end
-
   def recent_comments
     comments.limit(5).order(created_at: :desc)
+  end
+
+  after_save :updates_posts_counter
+
+  private
+
+  def updates_posts_counter
+    posts.increment!(:Posts_Counter)
   end
 end
