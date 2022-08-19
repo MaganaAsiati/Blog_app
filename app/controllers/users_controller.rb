b98a245d3ac01079ed445a5899946de54f54e0fa
MaganaAsiati <assia.magana@gmail.com>
2022-08-19 22:24:21 +0200

    create an instance variable for showing all users  and to display all posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
-  def index
-    @user = User.all 
- end
 
-  def show; end
+  def index
+      @users = User.order(:Name)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @recent_posts = @user.recent_posts
+  end
 end
