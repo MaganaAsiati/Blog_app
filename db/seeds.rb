b46928d8ca91745ad83bec84c7b47a47519d3dd7
MaganaAsiati <assia.magana@gmail.com>
2022-08-19 21:51:38 +0200

    remove all data inserte in all tables and restart insert data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create!(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.')
-User.create!(Name: 'Lilly', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Poland.')
+# assia = User.create!(Name: 'Assia', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.')
+# cedric = User.create!(Name: 'Cedric', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Poland.')
+# madiha = User.create!(Name: 'Madiha', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Ethiopie.')
+# juliette = User.create!(Name: 'Juliette', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Poland.')
 
-Post.create!(author_id: 1, Title: '1 Post ', Text: 'This is my first post Justo sit dolor gubergren magna rebum amet gubergren')
-Post.create!(author_id: 1, Title: '2 post', Text: 'This is my second post asd sadipscing dolores lorem at rebum amet dolore ..')
-Post.create!(author_id: 2, Title: '3 post', Text: 'This is my third post asd sadipscsadipscing dolores lorem at rebum amet dolore consetetur ')
-Post.create!(author_id: 2, Title: '4 post', Text: 'This is my fourth post asd sadipscing dolores lorem at rebum amet dolore ')
+# post_1 = Post.create!(author: assia , Title: '1 Post ', Text: 'This is my first post Justo sit dolor gubergren magna rebum amet gubergren')
+# post_2 = Post.create!(author: cedric, Title: '2 post', Text: 'This is my second post asd sadipscing dolores lorem at rebum amet dolore ..')
+# post_3 = Post.create!(author: assia, Title: '3 post', Text: 'This is my third post asd sadipscsadipscing dolores lorem at rebum amet dolore consetetur ')
+# post_4 = Post.create!(author: madiha, Title: '4 post', Text: 'This is my fourth post asd sadipscing dolores lorem at rebum amet dolore ')
+# post_5 = Post.create!(author: assia , Title: '5 Post ', Text: 'This is my first post Justo sit dolor gubergren magna rebum amet gubergren')
+# post_6 = Post.create!(author: juliette, Title: '6 post', Text: 'This is my second post asd sadipscing dolores lorem at rebum amet dolore ..')
+# post_7 = Post.create!(author: cedric, Title: '7 post', Text: 'This is my third post asd sadipscsadipscing dolores lorem at rebum amet dolore consetetur ')
+# post_8 = Post.create!(author: assia, Title: '8 post', Text: 'This is my fourth post asd sadipscing dolores 
 
-Comment.create!(post_id: 1, author_id: 2, text: 'Hi Tom! nice post')
-Comment.create!(post_id: 1, author_id: 1, text: 'Hi Lilly! This is awesome')
-Comment.create!(post_id: 2, author_id: 1, text: 'Hi Lilly! I can\'t see the share button')
-Comment.create!(post_id: 2, author_id: 2, text: 'Hi Tom! when are you publishing the next one')
-Comment.create!(post_id: 3, author_id: 1, text: 'Hi Lilly! this is so inspiring')
-Comment.create!(post_id: 3, author_id: 2, text: 'Hi Tom! I like this')
-Comment.create!(post_id: 4, author_id: 1, text: 'Hi Lilly! Amazing work')
-Comment.create!(post_id: 4, author_id: 2, text: 'Hi Tom! I need more of such writings') 
+# Comment_1 = Comment.create!(post: post_1, author: assia, text: 'Hi Tom! nice post')
+# Comment_2 = Comment.create!(post: post_3, author: assia, text: 'Hi Lilly! This is awesome')
+# Comment_3 = Comment.create!(post: post_3, author: cedric, text: 'Hi Lilly! I can\'t see the share button')
+# Comment_4 = Comment.create!(post: post_1, author: madiha, text: 'Hi Tom! when are you publishing the next one')
+# Comment_5 = Comment.create!(post: post_4, author: juliette, text: 'Hi Lilly! this is so inspiring')
+# Comment_6 = Comment.create!(post: post_2, author: assia, text: 'Hi Tom! I like this')
+# Comment_7 = Comment.create!(post: post_4, author: cedric, text: 'Hi Lilly! Amazing work')
+# Comment_8 = Comment.create!(post: post_1, author: madiha, text: 'Hi Tom! I need more of such writings') 
 
 
 
