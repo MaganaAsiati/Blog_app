3fd5299d0aeb198afd8b9e4c2106868eabbb5c21
MaganaAsiati <assia.magana@gmail.com>
2022-08-27 20:38:34 +0200

    fix linter errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,16 +4,18 @@ class Api::V1::UsersController < ApplicationController
     @users = User.all
     render json: @users, status: :ok
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
       render json: @user, status: :created
     else
-      render json: {message: "user not created"}, status: :unprocessable_entity
+      render json: { message: 'user not created' }, status: :unprocessable_entity
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:Name, :email, :password)
   end
