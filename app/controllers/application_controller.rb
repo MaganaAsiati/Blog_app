6ce465d1b63519d8f3dbd3e7fba78e01577606bc
MaganaAsiati <assia.magana@gmail.com>
2022-08-27 15:51:56 +0200

    add protect in the config/application file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  protect_from_forgery with: :null_session, if: proc {|c| c.request.format =~ %r{application/json;}}
   # def after_sign_in_path_for(resource_or_scope)
   #   users_path
   # end
