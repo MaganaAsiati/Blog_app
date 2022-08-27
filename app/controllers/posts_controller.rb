5eddfc600b7c32120102655f276bf6d02c17b633
MaganaAsiati <assia.magana@gmail.com>
2022-08-27 02:42:48 +0200

    add method to destroy post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,40 @@
+
+# before_action :set_post, only: [:index, :show, :destroy]
+
 class PostsController < ApplicationController
+  load_and_authorize_resource
   def index
     @user = User.find(params[:user_id])
-    @user_posts = @user.recent_posts
+    @user_posts = @user.posts.includes(:author)
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
     @comment = Comment.new
     @like = Like.new
   end
+  
+  def destroy
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+    puts 'Yes I can destroy'
+    @post.destroy 
+    redirect_to user_post_path(@user.id, @post.id)
+  end
 
   def new
+    @user = User.find(params[:user_id])
     @post = Post.new
     @params = params
   end
 
   def create
+    @user = User.find(params[:user_id])
     @post = current_user.posts.new(post_params)
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post }
+        format.html { redirect_to user_posts_url(@user.id) }
       else
         format.html { render :new }
       end
